896f48b1d55490c0dbefbfd88d0b91eaa307ee19
Kannan <kannadeveloper1771@gmail.com>
2025-11-15 21:15:51 +0530

    subPartUpdatedLoop

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 console.log("Hello")
 
-i=0
-while(i<10){
-    console.log(i);
-    i++;
+var a = [0,1,2,4,5,6]
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element)
 }
-
-console.log(i);
-
